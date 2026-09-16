c2058b86a6aa5fe22812444393ed69b628c1d382
tushi1827-cyber <tushi.1827@gmail.com>
2026-09-16 09:26:11 +0530

    Updated index.ts

--- supabase/functions/send-email/index.ts
+++ supabase/functions/send-email/index.ts
@@ -2,8 +2,8 @@ import { createClient } from "npm:@supabase/supabase-js@2.57.4";
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Client-Info, Apikey",
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
+  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
 };
 
 const RESEND_API_KEY = Deno.env.get("RESEND_API_KEY");
